e43d7496e7831794345dff98183ce9d56961a805
CATALIN ZAHARIEA <zahariea.c@gmail.com>
2021-03-23 14:03:16 +0100

    Exercise upload.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var userNumberInput = parseInt(prompt("Inserica un numero"));
+var userNumberList = [];
+
+for (var i = 1; i <= userNumberInput; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    userNumberList.push("Fizzbuzz");
+  } else if (i % 5 == 0) {
+    userNumberList.push("Buzz");
+  } else if (i % 3 == 0) {
+    userNumberList.push("Fizz");
+  } else {
+    userNumberList.push(i);
+  }
+}
+
+document.getElementById("listFizzBuzz").style.maxWidth = "990px";
+document.getElementById(
+  "listFizzBuzz"
+).innerHTML = `Ecco la sua lista FizzBuzz: ${userNumberList}`;
+console.log(`Ecco la sua lista FizzBuzz: ${userNumberList}`);
